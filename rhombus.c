1431f9881684b657e714ab073f6e0ed25213eacf
architsinghal0005 <architsinghal941@gmail.com>
2025-10-09 22:48:05 +0530

    rhombus.c

---
+++ rhombus.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, k;
+    for (i = 1; i <= 4; i++)
+    {
+        for (j = 1; j <= 3 + 1 - i; j++)
+        {
+            printf(" ");
+        }
+        for (k = 1; k <= 4; k++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
