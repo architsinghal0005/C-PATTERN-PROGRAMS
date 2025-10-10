d57855182b8387c2da9d67138adc956a96a636fb
architsinghal0005 <architsinghal941@gmail.com>
2025-10-10 19:55:57 +0530

    hourglass.c

---
+++ hourglass.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j, k;
+    printf("enter the number of lines :");
+    scanf("%d", &n);
+    for (i = n; i >= 1; i--)
+    {
+        for (j = n; j > i; j--)
+        {
+            printf(" ");
+        }
+
+        for (j = 1; j <= 2 * i - 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for (i = 2; i <= n; i++)
+    {
+        for (j = n; j > i; j--)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= 2 * i - 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
