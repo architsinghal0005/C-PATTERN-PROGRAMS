177467f6642284567d586e4dd46c2b809b4d68b5
architsinghal0005 <architsinghal941@gmail.com>
2025-10-11 18:27:43 +0530

    mazebasics,c
    maze will start from 1 to the given number by user

---
+++ mazebasic.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j, k;
+    printf("enter the number for which you want in maze :");
+    scanf("%d", &n);
+    int min = 0;
+    for (i = 1; i <= 2 * n - 1; i++)
+    {
+        for (j = 1; j <= 2 * n - 1; j++)
+        {
+            int a = i;
+            if (i > n)
+                a = 2 * n - i;
+            int b = j;
+            if (j> n)
+                b = 2 * n - j;
+            if (a < b)
+                min = a;
+            else
+                min = b;
+            printf("%d", min);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
